5fabfad9fce47e23ddfdde67262e399f3ecf899d
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-01-27 16:30:23 +0000

    Creatin resilient client to retry downloads after failures. This will not be used in prod as we are trying to measure data transfer accurately

--- client/index.js
+++ client/index.js
@@ -4,51 +4,70 @@ const fs = require('fs');
 const path = require("path")
 const textLog = require("./utils/textLog")
 const log = require("./utils/logger.js")
+const isReachable = require("is-reachable");
 
-const downloadFile = async (url, dest, cb) => {
+const downloadFile = async (url, dest) => {
     var file = fs.createWriteStream(dest);
-    http.get(url, (res) => {
-        res.pipe(file);
-        file.on('finish', () => {
-            file.end();
-            cb()
-        });
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            res.pipe(file);
+            file.on('finish', () => {
+                file.end();
+                resolve()
+            });
+        })
+            .on('error', (err) => {
+                reject(err)
+                fs.unlinkSync(dest)
+            })
     })
-    .on('error', () => {
-        fs.unlink(dest)
-    });
 };
 
-const downloadSingleServer = async (hostname) => {
-    let url = `http://${hostname}/download`
-    
-    count = 0;
+const isHostOnline = (hostname, cb) => {
+    log.debug("Checking if host is online")
     let interval = setInterval(async () => {
-        log.info(`Downloading: ${count} from ${hostname}`)
-        dest = path.join(__dirname, "downloads", `${hostname}-${count}.bin`)
-
-        await downloadFile(url, dest, {
-            success: () => {
-                textLog(path.join(__dirname, "logs", "downloads.log"),
-                    {testName: process.env.TEST_NAME, event: "DownloadSuccess", downloadHostname: hostname})
-                log.info(`Completed: ${count} from ${hostname}`)
-            }, 
-            error: () => {
-                textLog(path.join(__dirname, "logs", "downloads.log"),
-                    {testName: process.env.TEST_NAME, event: "DownloadFailed", downloadHostname: hostname})
-                log.warn(`Failed: ${count} from ${hostname}`)
-            }
-        })
-        
-        if (++count == process.env.NUM_DOWNLOADS) {
+        if (await isReachable(`${hostname}:80`)) {
+            log.debug("Host online: starting downloads")
             clearInterval(interval)
+            cb()
         }
-    }, 1000*10)
+        else
+            log.warn(`host ${hostname} is not reachable. Trying again`)
+    }, 3000)
 }
 
-const main = () => {
+const downloadXFiles = (hostname, count, numDownloads) => {
+    dest = path.join(__dirname, "downloads", `${hostname}-${count}.bin`)
+    let url = `http://${hostname}/download`
+    
+    if (count == numDownloads) {
+        return
+    }
+
+    isHostOnline(hostname, () => {
+        log.info(`Downloading: ${count} from ${hostname}`)
+        downloadFile(url, dest)
+            .then((res) => {
+                textLog(path.join(__dirname, "logs", "downloads.log"),
+                    { testName: process.env.TEST_NAME, event: "DownloadSuccess", downloadHostname: hostname })
+                log.info(`Completed: ${count} from ${hostname}`)
+                downloadXFiles(hostname, ++count, numDownloads)
+            })
+            .catch((err) => {
+                log.error(err)
+                textLog(path.join(__dirname, "logs", "downloads.log"),
+                    { testName: process.env.TEST_NAME, event: "DownloadFailed", downloadHostname: hostname })
+                log.warn(`Failed: ${count} from ${hostname}`)
+                downloadXFiles(hostname, count, numDownloads)
+            })
+    })
+}
+
+const main = async () => {
     // TODO: Handle multiple hosts at the same time
-    downloadSingleServer(process.env.DOWNLOAD_HOSTNAME)
+    isHostOnline(process.env.DOWNLOAD_HOSTNAME, () => {
+        downloadXFiles(process.env.DOWNLOAD_HOSTNAME, 0, process.env.NUM_DOWNLOADS)
+    })
 }
 
 main()
