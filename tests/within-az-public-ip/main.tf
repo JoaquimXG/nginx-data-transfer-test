ad8bc95b04e478f55274e36e627c27c268beebcf
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-01-28 22:24:04 +0000

    Creating test-3 within-az-public-ip

---
+++ tests/within-az-public-ip/main.tf
@@ -0,0 +1,66 @@
+terraform {
+	backend "s3" {
+		bucket = "xjg.terraformstate"
+		key = "within-az-public-ip.tfstate"
+		region = "eu-west-2"
+		dynamodb_table = "terraform-state"
+	}
+}
+
+variable default_tags {
+    default = {
+        Project = "NGINX-Test"
+        env = "Test"
+    }
+}
+
+provider aws {
+    profile = "personal"
+    region = "eu-west-2"
+
+    default_tags {
+        tags = var.default_tags
+    }
+}
+
+module nginx {
+    source = "../nginx/terraform-module"
+
+    tag_name = "t3-nginx"
+    region = "eu-west-2"
+    az = "eu-west-2a"
+    vpc_id = "vpc-18d09270"
+    subnet_id = "subnet-f012828a"
+    subdomain = "nginx-test"
+    security_group_id = "sg-04ae5e949212df7db"
+    reverse_proxy_target = module.server.public_ip
+    playbook_path = "../nginx/playbook.yml"
+
+}
+
+module server {
+    source = "../server/terraform-module"
+
+    tag_name = "server-test"
+    region = "eu-west-2"
+    az = "eu-west-2a"
+    vpc_id = "vpc-18d09270"
+    subnet_id = "subnet-f012828a"
+    subdomain = "server-test"
+    security_group_id = "sg-04ae5e949212df7db"
+    playbook_path = "../server/playbook.yml"
+}
+
+output server {
+    value = {
+        ip = "${module.server.public_ip}"
+        dns = "${module.server.dns}"
+    }
+}
+
+output nginx {
+    value = {
+        ip = "${module.nginx.public_ip}"
+        dns = "${module.nginx.dns}"
+    }
+}
