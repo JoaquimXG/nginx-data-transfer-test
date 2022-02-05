e98c19ce79a9e4e6a08271cd2e00c9006a70c32a
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-02-05 16:06:14 +0000

    Generalising tests

--- tests/1. within-az-private-ip/main.tf
+++ tests/1. within-az-private-ip/main.tf
@@ -1,17 +1,21 @@
 terraform {
 	backend "s3" {
 		bucket = "xjg.terraformstate"
-		key = "within-az-private-ip.tfstate"
+		key = "1-within-az-private-ip.tfstate"
 		region = "eu-west-2"
 		dynamodb_table = "terraform-state"
 	}
+}
+
+variable transfer_test {
+    default = "1"
 }
 
 variable default_tags {
     default = {
         Project = "NGINX-Test"
         env = "Test"
-        transfer_test = "1"
+        transfer_test = var.transfer_test
     }
 }
 
@@ -35,12 +39,12 @@ module nginx {
     security_group_id = "sg-04ae5e949212df7db"
     playbook_path = "../../nginx/playbook.yml"
     ansible_vars = {
-        public_ip = module.server.public_ip
+        public_ip = module.server.private_ip
     }
 
     dns = {
         domain = "joaquimgomez.com"
-        subdomain = "nginx-test"
+        subdomain = "test-${var.transfer_test}-nginx"
         ttl = 60
     }
 }
@@ -53,19 +57,18 @@ module server {
     az = "eu-west-2a"
     vpc_id = "vpc-18d09270"
     subnet_id = "subnet-f012828a"
-
-    dns = {
-        domain = "joaquimgomez.com"
-        subdomain = "server-test"
-        ttl = 60
-    }
     security_group_id = "sg-04ae5e949212df7db"
-
     playbook_path = "../../server/playbook.yml"
     ansible_vars = {
         HTTP_PORT = "80"
         FILE_NAME = "random.bin"
-        TEST_NAME = "test"
+        TEST_NAME = "${var.transfer_test}"
+    }
+
+    dns = {
+        domain = "joaquimgomez.com"
+        subdomain = "test-${var.transfer_test}-server"
+        ttl = 60
     }
 }
 
