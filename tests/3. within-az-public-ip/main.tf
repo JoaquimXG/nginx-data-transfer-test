ffa4c02f98d7b66922a0589937df41a47e036e8b
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-02-05 16:06:21 +0000

    Creating test 3

--- tests/3. within-az-public-ip/main.tf
+++ tests/3. within-az-public-ip/main.tf
@@ -1,16 +1,21 @@
 terraform {
 	backend "s3" {
 		bucket = "xjg.terraformstate"
-		key = "within-az-public-ip.tfstate"
+		key = "3-within-az-public-ip.tfstate"
 		region = "eu-west-2"
 		dynamodb_table = "terraform-state"
 	}
+}
+
+variable transfer_test {
+    default = "3"
 }
 
 variable default_tags {
     default = {
         Project = "NGINX-Test"
         env = "Test"
+        transfer_test = var.transfer_test
     }
 }
 
@@ -24,7 +29,7 @@ provider aws {
 }
 
 module nginx {
-    source = "../nginx/terraform-module"
+    source = "github.com/joaquimxg/tf-instance-module"
 
     tag_name = "t3-nginx"
     region = "eu-west-2"
@@ -33,22 +38,40 @@ module nginx {
     subnet_id = "subnet-f012828a"
     subdomain = "nginx-test"
     security_group_id = "sg-04ae5e949212df7db"
-    reverse_proxy_target = module.server.public_ip
     playbook_path = "../nginx/playbook.yml"
+    ansible_vars = {
+        public_ip = module.server.public_ip
+    }
+
+    dns = {
+        domain = "joaquimgomez.com"
+        subdomain = "test-${var.transfer_test}-nginx"
+        ttl = 60
+    }
 
 }
 
 module server {
-    source = "../server/terraform-module"
+    source = "github.com/joaquimxg/tf-instance-module"
 
     tag_name = "t3-server"
     region = "eu-west-2"
     az = "eu-west-2a"
     vpc_id = "vpc-18d09270"
     subnet_id = "subnet-f012828a"
-    subdomain = "server-test"
     security_group_id = "sg-04ae5e949212df7db"
     playbook_path = "../server/playbook.yml"
+    ansible_vars = {
+        HTTP_PORT = "80"
+        FILE_NAME = "random.bin"
+        TEST_NAME = "${var.transfer_test}"
+    }
+
+    dns = {
+        domain = "joaquimgomez.com"
+        subdomain = "test-${var.transfer_test}-server"
+        ttl = 60
+    }
 }
 
 output server {
