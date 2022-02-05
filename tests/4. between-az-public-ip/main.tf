b5855934c02ebc2e77b6eaf2fb3ea6c44a2d1e4c
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-02-05 16:15:15 +0000

    creating test 5

--- tests/4. between-az-public-ip/main.tf
+++ tests/4. between-az-public-ip/main.tf
@@ -1,7 +1,7 @@
 terraform {
 	backend "s3" {
 		bucket = "xjg.terraformstate"
-		key = "4-between-az-private-ip.tfstate"
+		key = "4-between-az-public-ip.tfstate"
 		region = "eu-west-2"
 		dynamodb_table = "terraform-state"
 	}
